577769320b2f4cf86e48694a67322b56b660b5e7
سبتيان <septianhadinata771@gmail.com>
2026-05-09 22:33:35 +0700

    Create contributors.js

---
+++ api/contributors.js
@@ -0,0 +1,19 @@
+import clientPromise from './_lib/mongodb.js';
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db('idglitxh');
+  const contributors = db.collection('contributors');
+
+  if (req.method === 'GET') {
+    const all = await contributors.find({}).toArray();
+    // Hapus password dari response
+    const safe = all.map(c => {
+      delete c.pass;
+      return c;
+    });
+    return res.status(200).json({ success: true, data: safe });
+  }
+
+  return res.status(405).json({ success: false, error: 'Method not allowed' });
+}
