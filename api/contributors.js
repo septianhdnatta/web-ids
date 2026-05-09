c21c6d01ce234497d224237143666d7d2c753367
سبتيان <septianhadinata771@gmail.com>
2026-05-10 00:43:25 +0700

    Update contributors.js

--- api/contributors.js
+++ api/contributors.js
@@ -7,7 +7,6 @@ export default async function handler(req, res) {
 
   if (req.method === 'GET') {
     const all = await contributors.find({}).toArray();
-    // Hapus password dari response
     const safe = all.map(c => {
       delete c.pass;
       return c;
